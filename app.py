ceac9a41dde1c67f9f2bffe5935d518b0bda5f29
Diego Fonseca <dfonseca404@gmail.com>
2018-01-21 15:06:31 -0400

    Implementing template with Flask

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+APP_NAME = 'Python Quotes API'
 
 @app.route('/')
 def home():
-    return "Hello World"
+    try:
+        return render_template('index.html', app_name=APP_NAME)
+    except Exception as e:
+        return str(e)
 
+@app.route('/about')
+def about():
+    try:
+        return render_template('about.html', app_name=APP_NAME)
+    except Exception as e:
+        return str(e)
+
+@app.errorhandler(404)
+def four_oh_four(e):
+    return render_template('error/404.html', app_name=APP_NAME, error = e)
 
 if __name__ == "__main__":
     app.run()
